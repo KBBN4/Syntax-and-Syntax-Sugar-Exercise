54b4bbba085f4169856bb47a71614bbd5376f054
katherine Buendia <kbbuendia24@gmail.com>
2023-04-13 12:32:26 -0400

    Syntax excercise

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,42 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // inferred typing
+            var answer = 4;
+            string  response; //int.Parse(Console.ReadLine()); // unable to initiate without defining var response 
+
+
+
+            if (answer < 9)
+            {
+                response = answer + " is less than nine";
+            }
+            else
+            {
+                response = answer + "is greater than or equal to nine";
+            }
+
+            Console.WriteLine(response);
+
+            /*
+            // string interpolation
+
+
+            Console.WriteLine($" Hi , your {answer} is less than my {response}. Try again.");
+
+            // ternary operator
+
+
+            var message = (response < answer) ? " your answer is less than mine" : "try again";
+            Console.WriteLine(message);
+            */
+
+
         }
+         
+
+           
+       
+
     }
 }
